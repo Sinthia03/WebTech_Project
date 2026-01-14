9bd60fa552f675f2246e5d629b77d8e7851ef419
Sinthia03 <siddikasinthia@gmail.com>
2026-01-14 13:28:40 +0600

    updatedpass

--- VIEW/UpdatePassword.php
+++ VIEW/UpdatePassword.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>update</title>
-     <link rel="stylesheet" href="../CSS/UpdatePassword.css">
+     <link rel="stylesheet" href="../CSS/Updatepassword.css">
 </head>
 <body>
    <div class="password-card">
